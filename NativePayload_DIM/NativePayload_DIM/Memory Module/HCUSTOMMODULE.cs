019847b1c846ace8406fbf83c20c755bd98df789
Damon Mohammadbagher <DamonMohammadbagher@users.noreply.github.com>
2023-05-26 14:35:24 -0400

    Add files via upload

---
+++ NativePayload_DIM/NativePayload_DIM/Memory Module/HCUSTOMMODULE.cs
@@ -0,0 +1,13 @@
+﻿namespace Scavanger.MemoryModule
+{
+    internal unsafe struct HCoustomMudule
+    {
+        public void* Value { get; set; }
+
+        public HCoustomMudule(void* ptr) => Value = ptr;
+
+        public static implicit operator HCoustomMudule(void* value) => new HCoustomMudule(value);
+
+        public static implicit operator void* (HCoustomMudule value) => value.Value;
+    }
+}
